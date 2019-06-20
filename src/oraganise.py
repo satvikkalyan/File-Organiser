ae9cc38f994f56fa7487c724abe9cf9737c6bdfb
Satvik Kalyan <satvikkalyan@gmail.com>
2019-06-20 22:28:58 +0530

    added moving capability

--- src/oraganise.py
+++ src/oraganise.py
@@ -1,16 +1,31 @@
 import os
-
-current_dir=os.getcwd()
-dir_files=os.listdir(current_dir)[1:]
+import shutil
+# current_dir=os.path.dirname(os.path.realpath(__file__))
+# print(current_dir)
+files_dir=r'D:\File Oragniser\File-Organiser\unordered Files'
+dest_dir=r'D:\File Oragniser\File-Organiser\Ordered Files'
+dir_files=os.listdir(files_dir)
 given_file_extensions=['.py','.txt','.docx']
 files_naming={}
 files_naming[".py"]="python files"
 files_naming[".txt"]="text Document"
 files_naming["Pdf"]="PDF Files"
+
 for file in dir_files:
+    try:
+        filename, file_extension = os.path.splitext(file)
+        if(file_extension in given_file_extensions):
+            dirName=dest_dir+"\\"+files_naming[file_extension]
+            if not os.path.exists(dirName):
+                os.mkdir(dirName)
+        else:
+            dirName = dest_dir + "\\" + "Other Files"
+            if not os.path.exists(dirName):
+                os.mkdir(dirName)
+        print(files_dir +"\\"+file, dirName)
+        shutil.move(files_dir + file, dirName)
 
-    filename, file_extension = os.path.splitext(file)
-    print("File Name :: "+filename+"\nFile Extension :: "+file_extension)
-
+    except:
+        print(file)
 
 
