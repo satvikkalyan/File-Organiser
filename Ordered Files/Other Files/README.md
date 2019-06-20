616b1a702816de001eca8109f0714e4dd4cd6fef
Satvik Kalyan <satvikkalyan@gmail.com>
2019-06-21 00:21:14 +0530

    Added Gui

--- Ordered Files/Other Files/README.md
+++
@@ -1,2 +0,0 @@
-# File-Organiser
-An application which helps user to organise their files in a systemic manner and also groups the files based on its type
--- src/oraganise.py
+++ src/oraganise.py
@@ -1,32 +1,36 @@
 import os
 import shutil
-# current_dir=os.path.dirname(os.path.realpath(__file__))
-# print(current_dir)
-files_dir=r'D:\File Oragniser\File-Organiser\unordered Files'
-dest_dir=r'D:\File Oragniser\File-Organiser\Ordered Files'
-dir_files=os.listdir(files_dir)
-given_file_extensions=['.py','.txt','.docx']
+
 files_naming={}
 files_naming[".py"]="python files"
 files_naming[".txt"]="text Document"
-files_naming["Pdf"]="PDF Files"
+files_naming[".pdf"]="PDF Files"
+files_naming[".docx"]="Word Documents"
+given_file_extensions = ['.py', '.txt', '.docx','.pdf']
 
-for file in dir_files:
-    try:
-        filename, file_extension = os.path.splitext(file)
-        if(file_extension in given_file_extensions):
-            dirName=dest_dir+"\\"+files_naming[file_extension]
-            if not os.path.exists(dirName):
-                os.mkdir(dirName)
-        else:
-            dirName = dest_dir + "\\" + "Other Files"
-            if not os.path.exists(dirName):
-                os.mkdir(dirName)
-        if not os.path.exists(dirName+"\\"+file):
-            shutil.move(files_dir +"\\"+file, dirName)
-        else:
-            print("File Already exists")
-    except:
-        print(file)
+def print_names():
+    print(files_naming)
+
+def move_files(list_paths):
+    files_dir=list_paths[0]
+    dest_dir=list_paths[1]
+    dir_files = os.listdir(files_dir)
+    for file in dir_files:
+        try:
+            filename, file_extension = os.path.splitext(file)
+            if(file_extension in files_naming):
+                dirName=dest_dir+"\\"+files_naming[file_extension]
+                if not os.path.exists(dirName):
+                    os.mkdir(dirName)
+            else:
+                dirName = dest_dir + "\\" + "Other Files"
+                if not os.path.exists(dirName):
+                    os.mkdir(dirName)
+            if not os.path.exists(dirName+"\\"+file):
+                shutil.move(files_dir +"\\"+file, dirName)
+            else:
+                print("File Already exists")
+        except:
+            print("Error in the File Named :: "+file)
 
 
